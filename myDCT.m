48a7aa110cfb786e408265493dc4fd6593bd8247
patniharshit <patniharshit@gmail.com>
2017-03-31 23:36:34 +0530

    add functions for idct, quantization, dequantization

--- myDCT.m
+++ myDCT.m
@@ -2,6 +2,6 @@ function [ dctTransformed ] = myDCT( im, F )
 %myDCT outputs DCT transformed image
 
 dctTransformed = F*double(im)*F';
-%matTransformed = dct2(im);
+
 end
 
---
+++ myDCT_quantization.m
@@ -0,0 +1,12 @@
+function [ imqDCT ] = myDCT_quantization( imDCT, qm, c )
+%myDCT_quantization for outputs quantized DCT image
+
+imqDCT = ones(8,8);
+for i = 1:8
+    for j = 1:8
+        imqDCT(i,j) = round(imDCT(i,j) ./ (qm(i,j) .* c));
+    end
+end
+
+end
+
