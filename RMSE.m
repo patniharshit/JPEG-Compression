ff1b1d3c312bf675c46a2de11c09f45cd41482be
patniharshit <patniharshit@gmail.com>
2017-04-02 02:24:24 +0530

    Implement RMSE implicitly

--- RMSE.m
+++ RMSE.m
@@ -1,7 +1,15 @@
 function [ error ] = RMSE( im1, im2 )
-%RMSE calculates root mean square error between two images
+% RMSE calculates root mean square error between two images
+temp = ((double(im1)-double(im2)).^2)/length(im1);
+error = 0;
 
-error = sqrt(sum((im1 - im2).^2)/length(im1));
+for i = 1:512
+    for j = 1:512
+        error = error + temp(i,j);
+    end
+end
+error = error/numel(im1);
+error = sqrt(double(error));
 
 end
 
