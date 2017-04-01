cea747c81245bfec84575bd5b22841f6a17bec9b
patniharshit <patniharshit@gmail.com>
2017-04-02 00:05:47 +0530

    [q2]Add question 2

--- q2.m
+++ q2.m
@@ -1,7 +1,8 @@
 % q2
-% Apply DCT transform and quanitzation to LAKE.tif
+% Take some patches out of image and apply DCT, Quantization,
+% Dequantization, IDCT on them
 
-%Classical quantization matrix for luminance matrix
+%Quantization matrix for luminance
 Q=[16  11  10  16  24  40  51  61;
     12  12  14  19  26  58  60  55;
     14  13  16  24  40  57  69  56;
@@ -11,8 +12,51 @@ Q=[16  11  10  16  24  40  51  61;
     49  64  78  87  103 121 120 101;
     72  92  95  98  112 100 103 99];
 
+c = 2;
+
 %8-point 2d-DCT 
 F = create_mat_dct();
 
 im = imread('LAKE.TIF');
 [imx, imy] = size(im);
+
+clip1 = im(45:52, 420:427);
+dc1 = myDCT(clip1, F);
+quant1 = myDCT_quantization(dc1, Q, c);
+dequant1 = myDCT_dequantization(quant1, Q, c);
+idc1 = myIDCT(dequant1, F);
+figure;
+subplot(1,5,1); imshow(clip1); title('Image');
+subplot(1,5,2); imshow(dc1); title('DCT');
+subplot(1,5,3); imshow(quant1); title('Qunatized');
+subplot(1,5,4); imshow(dequant1); title('Dequantized');
+subplot(1,5,5); imshow(uint8(idc1)); title('IDCT');
+im(45:52, 420:427) = idc1;
+
+clip2 = im(298:305, 427:434);
+dc2 = myDCT(clip2, F);
+quant2 = myDCT_quantization(dc2, Q, c);
+dequant2 = myDCT_dequantization(quant2, Q, c);
+idc2 = myIDCT(dequant2, F);
+figure;
+subplot(1,5,1); imshow(clip2); title('Image');
+subplot(1,5,2); imshow(dc2); title('DCT');
+subplot(1,5,3); imshow(quant2); title('Qunatized');
+subplot(1,5,4); imshow(dequant2); title('Dequantized');
+subplot(1,5,5); imshow(uint8(idc2)); title('IDCT');
+im(298:305, 427:434) = idc2;
+
+clip3 = im(230:237, 30:37);
+dc3 = myDCT(clip3, F);
+quant3 = myDCT_quantization(dc3, Q, c);
+dequant3 = myDCT_dequantization(quant3, Q, c);
+idc3 = myIDCT(dequant3, F);
+figure;
+subplot(1,5,1); imshow(clip3); title('Image');
+subplot(1,5,2); imshow(dc3); title('DCT');
+subplot(1,5,3); imshow(quant3); title('Qunatized');
+subplot(1,5,4); imshow(dequant3); title('Dequantized');
+subplot(1,5,5); imshow(uint8(idc3)); title('IDCT');
+im(230:237, 30:37) = idc3;
+
+figure; imshow(im);
