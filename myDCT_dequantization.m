2413806d7e5c9740e9a65c361a1e9a581cec8e4c
patniharshit <patniharshit@gmail.com>
2017-04-01 19:56:35 +0530

    [q1]Exchange loop variables

--- myDCT_dequantization.m
+++ myDCT_dequantization.m
@@ -2,8 +2,8 @@ function [ dequantizedImage ] = myDCT_dequantization(imqDCT, qm, c)
 %myDCT_dequantization de-quantizes the quantized DCT image
 
 dequantizedImage = ones(8,8);
-for i = 1:8
-    for j = 1:8
+for j = 1:8
+    for i = 1:8
         dequantizedImage(i,j) = round(imqDCT(i,j) .* (qm(i,j) .* c));
     end
 end
