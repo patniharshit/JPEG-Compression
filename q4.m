ff411a875dc807bb9624a1b018a4610520219a62
patniharshit <patniharshit@gmail.com>
2017-04-02 01:29:27 +0530

    [q4]Iterate for various values of c

--- q4.m
+++ q4.m
@@ -12,37 +12,40 @@ Q=[16  11  10  16  24  40  51  61;
     49  64  78  87  103 121 120 101;
     72  92  95  98  112 100 103 99];
 
-c = 10;
 F = create_mat_dct();
 
 im = imread('LAKE.TIF');
 [im_x, im_y] = size(im);
 
-q_im = zeros(im_x, im_y);
 
-for j = 1 : 8 : im_x
-   for i = 1 : 8 : im_y
-       temp_im = im(i:i+7, j:j+7);
-       % temp_im = temp_im - 128;
-       dct_im = myDCT(temp_im, F);
-       quant_im = myDCT_quantization(dct_im, Q, c);
-       q_im(i:i+7, j:j+7) = quant_im;
-   end
+for c = 1 : 5 : 40
+    q_im = zeros(im_x, im_y);
+
+    for j = 1 : 8 : im_x
+       for i = 1 : 8 : im_y
+           temp_im = im(i:i+7, j:j+7);
+           % temp_im = temp_im - 128;
+           dct_im = myDCT(temp_im, F);
+           quant_im = myDCT_quantization(dct_im, Q, c);
+           q_im(i:i+7, j:j+7) = quant_im;
+       end
+    end
+
+    % Dequantize
+    dq_im = zeros(im_x, im_y);
+
+    for j = 1 : 8 : im_x
+       for i = 1 : 8 : im_y
+           temp_im = q_im(i:i+7, j:j+7);
+           dequant_im = myDCT_dequantization(temp_im, Q, c);
+           idct_im = myIDCT(dequant_im, F);
+           % idct_im = idct_im + 128;
+           dq_im(i:i+7, j:j+7) = idct_im;
+       end
+    end
+
+    figure
+    subplot(1,2,1), imshow(uint8(im)); title('Original Image');
+    subplot(1,2,2), imshow(uint8(dq_im)); title(['For c = ' num2str(c) '  compressed image'])
 end
 
-% Dequantize
-dq_im = zeros(im_x, im_y);
-
-for j = 1 : 8 : im_x
-   for i = 1 : 8 : im_y
-       temp_im = q_im(i:i+7, j:j+7);
-       dequant_im = myDCT_dequantization(temp_im, Q, c);
-       idct_im = myIDCT(dequant_im, F);
-       % idct_im = idct_im + 128;
-       dq_im(i:i+7, j:j+7) = idct_im;
-   end
-end
-
-figure
-subplot(1,2,1), imshow(uint8(im)); title('Original Image');
-subplot(1,2,2), imshow(uint8(dq_im)); title('Compressed Image');
