df271757bc0ec773fde047974a71519ea75cb155
patniharshit <patniharshit@gmail.com>
2017-04-02 23:13:12 +0530

    [q1]Refactor RMSE function to work with all size arrays

--- RMSE.m
+++ RMSE.m
@@ -1,14 +1,15 @@
 function [ error ] = RMSE( im1, im2 )
 % RMSE calculates root mean square error between two images
-temp = ((double(im1)-double(im2)).^2)/length(im1);
+temp = ((double(im1)-double(im2)).^2)/(length(im1)*length(im1));
 error = 0;
+[x,y] = size(im1);
 
-for i = 1:512
-    for j = 1:512
+for i = 1:x
+    for j = 1:y
         error = error + temp(i,j);
     end
 end
-error = error/numel(im1);
+
 error = sqrt(double(error));
 
 end
