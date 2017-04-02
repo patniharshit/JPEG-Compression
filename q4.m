d5a8f3155247aaf1180425f670f3a04df929f3a1
patniharshit <patniharshit@gmail.com>
2017-04-02 23:16:27 +0530

    [q4]Done

--- q4.m
+++ q4.m
@@ -1,6 +1,17 @@
 % q3
 % Reconstructing the compressed image
 
+% COMMENTS
+% For c = 4 the distortions of the reconstructed image are just perceptible.
+% We can see that the Entropy goes on increasing as c increased initially 
+% and then drops later on as c increases. As c increases,
+% the image becomes distorted and nearby pixels start having more and more 
+% different values, so the entropy of the image increases earlier on, but 
+% later, all nearby pixels start attaining the same colour, so the entropy 
+% decreases.
+% The RMSE always increased as c increased, since the image became 
+% more and more distorted.
+
 % Firstly compress the image
 % Classical quantization matrix for luminance matrix
 Q=[16  11  10  16  24  40  51  61;
@@ -18,13 +29,12 @@ im = imread('LAKE.TIF');
 [im_x, im_y] = size(im);
 
 
-for c = 1 : 5 : 40
+for c = 1 : 20
     q_im = zeros(im_x, im_y);
 
     for j = 1 : 8 : im_x
        for i = 1 : 8 : im_y
            temp_im = im(i:i+7, j:j+7);
-           % temp_im = temp_im - 128;
            dct_im = myDCT(temp_im, F);
            quant_im = myDCT_quantization(dct_im, Q, c);
            q_im(i:i+7, j:j+7) = quant_im;
@@ -39,13 +49,15 @@ for c = 1 : 5 : 40
            temp_im = q_im(i:i+7, j:j+7);
            dequant_im = myDCT_dequantization(temp_im, Q, c);
            idct_im = myIDCT(dequant_im, F);
-           % idct_im = idct_im + 128;
            dq_im(i:i+7, j:j+7) = idct_im;
        end
     end
-
+    
+    fprintf('For c: %i\n', c);
+    fprintf('Entropy of image: %f\n', My_entropy(uint8(dq_im)));
+    fprintf('RMSE of image: %f\n\n', RMSE(im, dq_im));
+    
     figure
     subplot(1,2,1), imshow(uint8(im)); title('Original Image');
     subplot(1,2,2), imshow(uint8(dq_im)); title(['For c = ' num2str(c) '  compressed image'])
 end
-
